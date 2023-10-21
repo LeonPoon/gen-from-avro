80a5d2e048452fe6f8a865e56f26da258ca51f64
Leon Poon <szeleung.poon@gmail.com>
2023-10-21 23:18:00 +0800

    add test for enum

--- avsc-gen-scala/src/test/scala/com/codepowered/avro_based/avsc_gen_scala/test/AvscGenTesting.scala
+++ avsc-gen-scala/src/test/scala/com/codepowered/avro_based/avsc_gen_scala/test/AvscGenTesting.scala
@@ -9,6 +9,7 @@ import org.scalatest.matchers.should
 import treehuggerDSL._
 
 import java.nio.file.{Files, Path, Paths}
+import scala.jdk.CollectionConverters.SeqHasAsJava
 
 class AvscGenTesting extends AnyFlatSpec with should.Matchers {
 
@@ -69,28 +70,37 @@ class AvscGenTesting extends AnyFlatSpec with should.Matchers {
     }
   }
 
-  it should s"generate nothing for array schema" in {
+  it should "generate nothing for array schema" in {
     val schema = new Schema.Parser().parse(Schema.createArray(Schema.create(Schema.Type.INT)).toString(true))
-    val generated = new AvscGenScala(null, schema, s"gen.nothing.for_._array.MyAvroSchema").files
+    val generated = new AvscGenScala(null, schema, "gen.nothing.for_._array.MyAvroSchema").files
     AvscGenScala.toFiles(generateTo, generated)
     generated.tail shouldBe empty
     generated.head.generatedElement shouldBe UnitInfo(Some("gen.nothing.for_._array"), "MyAvroSchema")
   }
 
-  it should s"generate nothing for map schema" in {
+  it should "generate nothing for map schema" in {
     val schema = new Schema.Parser().parse(Schema.createMap(Schema.create(Schema.Type.INT)).toString(true))
-    val generated = new AvscGenScala(null, schema, s"gen.nothing.for_._map.MyAvroSchema").files
+    val generated = new AvscGenScala(null, schema, "gen.nothing.for_._map.MyAvroSchema").files
     AvscGenScala.toFiles(generateTo, generated)
     generated.tail shouldBe empty
     generated.head.generatedElement shouldBe UnitInfo(Some("gen.nothing.for_._map"), "MyAvroSchema")
   }
 
-  it should s"generate for fixed schema" in {
+  it should "generate for fixed schema" in {
     val schema = new Schema.Parser().parse(Schema.createFixed("someFixed", null, "gen.for_._fixed", 10).toString(true))
-    val generated = new AvscGenScala(null, schema, s"gen.for_._fixed.MyAvroSchema").files
+    val generated = new AvscGenScala(null, schema, "gen.for_._fixed.MyAvroSchema").files
     AvscGenScala.toFiles(generateTo, generated)
     generated.head.generatedElement shouldBe UnitInfo(Some("gen.for_._fixed"), "MyAvroSchema")
     generated.tail.head.generatedElement shouldBe UnitInfo(Some("gen.for_._fixed"), "someFixed")
+    generated.tail.tail shouldBe empty
+  }
+
+  it should "generate enum" in {
+    val schema = new Schema.Parser().parse(Schema.createEnum("Enum1", null, "gen.for_._enum1", List("Apple", "Orange").asJava).toString())
+    val generated = new AvscGenScala(null, schema, "gen.for_._enum1.MyAvroSchema_enum1").files
+    AvscGenScala.toFiles(generateTo, generated)
+    generated.head.generatedElement shouldBe UnitInfo(Some("gen.for_._enum1"), "MyAvroSchema_enum1")
+    generated.tail.head.generatedElement shouldBe UnitInfo(Some("gen.for_._enum1"), "Enum1")
     generated.tail.tail shouldBe empty
   }
 
